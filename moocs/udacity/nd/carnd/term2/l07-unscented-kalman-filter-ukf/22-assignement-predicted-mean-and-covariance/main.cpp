f2c0f7503da530a88da01e15a83f8463124e9d5f
Kinshuk <kinshuk.ram@gmail.com>
2017-07-24 01:41:17 +0200

    Update carnd t2l7 ch22

---
+++ moocs/udacity/nd/carnd/term2/l07-unscented-kalman-filter-ukf/22-assignement-predicted-mean-and-covariance/main.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Matthew Zimmer on 4/1/17.
+//
+
+#include <iostream>
+#include "Eigen/Dense"
+#include <vector>
+#include "ukf.h"
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+int main() {
+
+  //Create a UKF instance
+  UKF ukf;
+
+/*******************************************************************************
+* Programming assignment calls
+*******************************************************************************/
+
+  VectorXd x_pred = VectorXd(5);
+  MatrixXd P_pred = MatrixXd(5, 5);
+  ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
+
+  return 0;
+}
