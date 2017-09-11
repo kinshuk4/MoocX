a41e7954fe3fda0c7418ef771f883086c945c3f7
Kinshuk <kinshuk.ram@gmail.com>
2017-09-11 22:22:56 +0200

    Update getAllRepos script for bitbucket

--- k2e/dev/tools/bitbucket/getAllRepos.sh
+++ k2e/dev/tools/bitbucket/getAllRepos.sh
@@ -4,10 +4,11 @@
 
 curl -u ${1}  https://api.bitbucket.org/1.0/users/${1} > repoinfo
 repoType="git"
-for repo_name in `grep \"name\" repoinfo | cut -f4 -d\"`
+for repo_name in `cat repoinfo | jq '.repositories[]' | jq .name | sed 's/\"//g'`
 do
-	$repoType clone https://$repoType@bitbucket.org/${1}/$repo_name
+	$repoType clone https://${1}@bitbucket.org/${1}/$repo_name
 done
 
 # Note: mercurial users can replace repoType to "hg"
-# Also, I am using "https" but we can also ssh if you don't need to use github desktop client
+# Also, I am using "https" but we can also ssh if you don't need to import the projec
+# $repoType clone ssh://$repoType@bitbucket.org/${1}/$repo_name
